38d8b49a6b695c4684af311f0acbc00117b0499d
Asutosh Das <asutoshd@codeaurora.org>
2014-03-21 11:23:59 +0530

    mmc: core: enable async suspend
    Enables the asynchronous suspend of card and class device.
    This would enable both these devices to suspend in separate
    respective threads, thus allowing other devices to suspend
    concurrently.
    
    Signed-off-by: Asutosh Das <asutoshd@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    
    Conflicts:
    	drivers/mmc/core/bus.c

--- drivers/mmc/core/bus.c
+++ drivers/mmc/core/bus.c
@@ -327,7 +327,7 @@ int mmc_add_card(struct mmc_card *card)
 	ret = device_add(&card->dev);
 	if (ret)
 		return ret;
-
+	device_enable_async_suspend(&card->dev);
 	mmc_card_set_present(card);
 
 	return 0;
