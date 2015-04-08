fffe2b019392ad47d77a72a3002bee6507241f24
Raymond Golo (intersectRaven) <rjgolo@gmail.com>
2015-04-08 15:29:38 +0800

    Reapply most recent ondemand modifications to hotplug.
    Modifications include the ff:
    - cpufreq: governors: Remove duplicate check of target freq in supported range
    - cpufreq: ondemand: Eliminate the deadband effect

--- drivers/cpufreq/cpufreq_hotplug.c
+++ drivers/cpufreq/cpufreq_hotplug.c
@@ -629,21 +629,21 @@ static void hp_check_cpu(int cpu, unsigned int load)
 		goto hp_check;	/* <-XXX */
 	} else {
 		/* Calculate the next frequency proportional to load */
-		unsigned int freq_next;
-		freq_next = load * policy->cpuinfo.max_freq / 100;
+		unsigned int freq_next, min_f, max_f;
+
+		min_f = policy->cpuinfo.min_freq;
+		max_f = policy->cpuinfo.max_freq;
+		freq_next = min_f + load * (max_f - min_f) / 100;
 
 		/* No longer fully busy, reset rate_mult */
 		dbs_info->rate_mult = 1;
 
-		if (freq_next < policy->min)
-			freq_next = policy->min;
-
 		if (!hp_tuners->powersave_bias) {
-			__cpufreq_driver_target(policy, freq_next, CPUFREQ_RELATION_L);
+			__cpufreq_driver_target(policy, freq_next, CPUFREQ_RELATION_C);
 		} else {
 			freq_next = hp_ops.powersave_bias_target(policy, freq_next,
 								 CPUFREQ_RELATION_L);
-			__cpufreq_driver_target(policy, freq_next, CPUFREQ_RELATION_L);
+			__cpufreq_driver_target(policy, freq_next, CPUFREQ_RELATION_C);
 		}
 	}
 /* <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< */
