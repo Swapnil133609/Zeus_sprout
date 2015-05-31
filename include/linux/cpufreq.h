5e2121ed7667951090f0639433819527ca6af2dd
Swapnil <swapnil133609@gmail.com>
2015-05-31 15:53:40 +0530

    Added Wheatley CPUFreq Governor

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -412,6 +412,9 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_lagfree)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_WHEATLEY)
+extern struct cpufreq_governor cpufreq_gov_wheatley;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_wheatley)
 #endif
 
 
