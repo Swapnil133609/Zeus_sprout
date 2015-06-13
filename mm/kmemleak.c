83196b6109d286577a9381bf98ddfb06a560c7c2
Swapnil <swapnil133609@gmail.com>
2015-06-13 19:39:04 +0530

    Revert "kmemleak: allow freeing internal objects after kmemleak was disabled"
    This reverts commit ef5611caa71c9a55827b333b9c047969e920ae70.
    
    	modified:   Documentation/kmemleak.txt
    	modified:   mm/kmemleak.c

--- mm/kmemleak.c
+++ mm/kmemleak.c
@@ -1594,8 +1594,6 @@ static void kmemleak_clear(void)
 	rcu_read_unlock();
 }
 
-static void __kmemleak_do_cleanup(void);
-
 /*
  * File write operation to configure kmemleak at run-time. The following
  * commands can be written to the /sys/kernel/debug/kmemleak file:
@@ -1608,8 +1606,7 @@ static void __kmemleak_do_cleanup(void);
  *		  disable it)
  *   scan	- trigger a memory scan
  *   clear	- mark all current reported unreferenced kmemleak objects as
- *		  grey to ignore printing them, or free all kmemleak objects
- *		  if kmemleak has been disabled.
+ *		  grey to ignore printing them
  *   dump=...	- dump information about the object found at the given address
  */
 static ssize_t kmemleak_write(struct file *file, const char __user *user_buf,
@@ -1619,6 +1616,9 @@ static ssize_t kmemleak_write(struct file *file, const char __user *user_buf,
 	int buf_size;
 	int ret;
 
+	if (!atomic_read(&kmemleak_enabled))
+		return -EBUSY;
+
 	buf_size = min(size, (sizeof(buf) - 1));
 	if (strncpy_from_user(buf, user_buf, buf_size) < 0)
 		return -EFAULT;
@@ -1627,19 +1627,6 @@ static ssize_t kmemleak_write(struct file *file, const char __user *user_buf,
 	ret = mutex_lock_interruptible(&scan_mutex);
 	if (ret < 0)
 		return ret;
-
-	if (strncmp(buf, "clear", 5) == 0) {
-		if (atomic_read(&kmemleak_enabled))
-			kmemleak_clear();
-		else
-			__kmemleak_do_cleanup();
-		goto out;
-	}
-
-	if (!atomic_read(&kmemleak_enabled)) {
-		ret = -EBUSY;
-		goto out;
-	}
 
 	if (strncmp(buf, "off", 3) == 0)
 		kmemleak_disable();
@@ -1664,6 +1651,8 @@ static ssize_t kmemleak_write(struct file *file, const char __user *user_buf,
 		}
 	} else if (strncmp(buf, "scan", 4) == 0)
 		kmemleak_scan();
+	else if (strncmp(buf, "clear", 5) == 0)
+		kmemleak_clear();
 	else if (strncmp(buf, "dump=", 5) == 0)
 		ret = dump_str_object_info(buf + 5);
 	else
@@ -1688,16 +1677,6 @@ static const struct file_operations kmemleak_fops = {
 	.release	= kmemleak_release,
 };
 
-static void __kmemleak_do_cleanup(void)
-{
-	struct kmemleak_object *object;
-
-	rcu_read_lock();
-	list_for_each_entry_rcu(object, &object_list, object_list)
-		delete_object_full(object->pointer);
-	rcu_read_unlock();
-}
-
 /*
  * Stop the memory scanning thread and free the kmemleak internal objects if
  * no previous scan thread (otherwise, kmemleak may still have some useful
@@ -1705,7 +1684,6 @@ static void __kmemleak_do_cleanup(void)
  */
 static void kmemleak_do_cleanup(struct work_struct *work)
 {
-<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
 	mutex_lock(&scan_mutex);
@@ -1741,16 +1719,6 @@ static void kmemleak_do_cleanup(struct work_struct *work)
 >>>>>>> parent of b1f3f50... kmemleak: free internal objects only if there're no leaks to be reported
 =======
 >>>>>>> parent of 236ca0e... kmemleak: allow freeing internal objects after kmemleak was disabled
-=======
-	mutex_lock(&scan_mutex);
-	stop_scan_thread();
-
-	if (!kmemleak_found_leaks)
-		__kmemleak_do_cleanup();
-	else
-		pr_info("Kmemleak disabled without freeing internal data. "
-			"Reclaim the memory with \"echo clear > /sys/kernel/debug/kmemleak\"\n");
->>>>>>> f139ccb... kmemleak: allow freeing internal objects after kmemleak was disabled
 	mutex_unlock(&scan_mutex);
 }
 
