c7e2a02fddadcf80fe40996f33233c46bbe3c978
Levin Calado <levincalado@gmail.com>
2015-06-29 22:43:59 +0800

    interactiveX: set suspendfreq value to minimum for MTK6582 SOCs
    Signed-off-by: Levin Calado <levincalado@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- drivers/cpufreq/cpufreq_interactiveX.c
+++ drivers/cpufreq/cpufreq_interactiveX.c
@@ -68,7 +68,7 @@ static struct mutex set_speed_lock;
 // used for suspend code
 static unsigned int enabled = 0;
 static unsigned int registration = 0;
-static unsigned int suspendfreq = 648000;
+static unsigned int suspendfreq = 598000;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
 static u64 hispeed_freq;
