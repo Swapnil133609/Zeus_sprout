b8669a65ab268e6bca356e7e08fc8e9bbc03785a
Swapnil Solanki <swapnil133609@gmail.com>
2015-07-28 13:59:17 +0530

    cpufreq: zzmoove: Bring up!
    Signed-off-by: Swapnil Solanki <swapnil133609@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -450,6 +450,9 @@ extern struct cpufreq_governor cpufreq_gov_impulse;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DARKNESS)
 extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_darkness)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_zzmoove)
 #endif
 
 
