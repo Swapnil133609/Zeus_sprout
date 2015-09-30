4cf3bccc6c5d82daa228d2f69e87c0a6ca410308
Levin Calado <levincalado@gmail.com>
2015-09-30 21:32:56 +0800

    pocket_mod: add pocket_mod.h
    Signed-off-by: Levin Calado <levincalado@gmail.com>

---
+++ include/linux/pocket_mod.h
@@ -0,0 +1,10 @@
+#ifndef _LINUX_POCKET_MOD_H
+#define _LINUX_POCKET_MOD_H
+
+extern int is_screen_on;
+
+int pocket_detection_check(void);
+
+int device_is_pocketed(void);
+
+#endif //_LINUX_POCKET_MOD_H
